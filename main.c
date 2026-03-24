18a08c8cadcea3bbacc7d04bdc8492611adbe5db
Paulo <paulo.alessandro05@gmail.com>
2026-03-24 14:50:49 -0300

    i did this without github setup, but well it's not like much happened. in the next projects i'll set up git from the start

---
+++ main.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <string.h>
+
+void get_info(char *filename, char *needle, char *data, int size){
+    FILE *pointer = fopen(filename, "r");
+    if (pointer == NULL) {
+        printf("Error Opening File \n");
+        return; //This is, if for some reason the file doesn't open, it exits gracefully
+    }
+
+    while(fgets(data, size, pointer) != NULL){
+        if(strstr(data, needle) != NULL){
+            break;
+            //To avoid overwriting, exits the loop on first match with "needle"
+        }
+    }
+    if (fclose(pointer) == EOF){
+        printf("Error Closing File");
+        return;
+    }
+    return;
+}
+
+//I realised that the conversions between kB/mB/gB followed the same patterns as s/n/h so i made it as a function, yay
+void convert_to_readable(int *small_unit, int *mid_unit, int *large_unit, int maximum_size){
+    if(*small_unit >= maximum_size){
+        *mid_unit = *small_unit / maximum_size;
+        *small_unit = *small_unit - (*mid_unit * maximum_size);
+        if(*mid_unit >= maximum_size){
+            *large_unit = *mid_unit / maximum_size;
+            *mid_unit = *mid_unit - (*large_unit * maximum_size);
+        }
+    }
+}
+
+void get_uptime(char *filename){
+    char data[100];
+    int hours = 0;
+    int minutes = 0;
+    int seconds = 0;
+
+    FILE *pointer = fopen(filename, "r");
+    if (pointer == NULL) {
+        printf("Error Opening File \n");
+        return; //same as the first function, probably should've separated this
+    }
+    fgets(data, sizeof(data), pointer);
+
+    //convertion to readable metrics incoming
+    sscanf(data, "%d", &seconds);
+    convert_to_readable(&seconds, &minutes, &hours, 60);
+    printf("Uptime: %d h %d m %d s \n", hours, minutes, seconds);
+
+    if (fclose(pointer) == EOF){
+        printf("Error Closing File");
+        return;
+    }
+    return;
+}
+
+
+int main() {
+    char name_needle[12] = "PRETTY_NAME";
+    char os_result [100];
+    char kernel_needle[14] = "Linux version";
+    char kernel_result[150];
+    char cpu_needle[11] = "model name";
+    char cpu_result[100];
+    char memory_needle[7] = "Active";
+    char memory_result[100];
+
+    //OS VERSION BLOCK - - - - - - - - - - - - - -
+    get_info("/etc/os-release", name_needle, os_result, sizeof(os_result));
+    char *os_result_cleanup = (strstr(os_result, "=")) + 2;
+    char *os_char_toRemove = strrchr(os_result, '"');
+    *os_char_toRemove = '\0';
+    printf("Operating System: %s \n", os_result_cleanup);
+
+    //KERNEL VERSION BLOCK - - - - - - - - - - -
+    get_info("/proc/version", kernel_needle, kernel_result, sizeof(kernel_result));
+    char *kernel_toRemovefrom = (strstr(kernel_result, "(")) - 1; //Best way i found to cut out the random data that follows the kernel release
+    *kernel_toRemovefrom = '\0';
+    char *kernel_cleanup = (strstr(kernel_result, "n ")) + 2; //best way i found to cut out "Linux Version"
+    printf("Kernel Version: %s \n", kernel_cleanup);
+
+    //CPU MODEL BLOCK - - - - - - - - - - - - -
+    get_info("/proc/cpuinfo", cpu_needle, cpu_result, sizeof(cpu_result));
+    char *cpu_cleanup = (strstr(cpu_result, ":")) + 2;
+    printf("CPU model: %s", cpu_cleanup);
+
+    //MEMORY USAGE BLOCK - - - - - - - - - - -
+    int kilobytes = 0;
+    int megabytes = 0;
+    int gigabytes = 0;
+    get_info("/proc/meminfo", memory_needle, memory_result, sizeof(memory_result));
+    sscanf(memory_result, "Active:          %d", &kilobytes); //this extracts the kilobytes value
+    convert_to_readable(&kilobytes, &megabytes, &gigabytes, 1024); //no using google to figure out your ram usage
+    printf("Memory Usage: %d GB %d MB %d KB \n", gigabytes, megabytes, kilobytes);
+
+    get_uptime("/proc/uptime"); //wow only one line for this one
+    return 0;
+}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+I wanted to learn C as a hobby, so i asked claude to give me a project to do (because im kind of familiar with programming logic, you know, multiple begginer courses that led to nothing). So it suggested me to do a simple version of neofetch from scratch. I requested explicetly to not give me code and only some logic hints if im stuck, so it's basically all me. no ai vibe code slop.
+To be my first C project i think i learned a lot in only like... 4 days it took me? just doing 1 or two hours at most per day. Definetly looking forward to the next one.
+
+Note: I programed this for fedora 44, which is the distro i'm using, i don't know if it will work in other distros. I'm pretty sure that in 2 years i'll look at this and say "disgusting".
