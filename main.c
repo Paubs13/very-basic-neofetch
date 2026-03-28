e42d1c6d7b004d3092c1fd8eb0031cd7f62ece95
paless13 <paulo.alessandro05@gmail.com>
2026-03-28 23:09:33 +0000

    added total memory and hostname, re structured the struct (wink wink) and small tweaks with comments and stuff that gave me TOC to look at

--- main.c
+++ main.c
@@ -6,17 +6,26 @@
 #define BCYN "\e[1;36m" // Bold Cyan
 
 typedef struct{
+    char hostname[50];
     char name[100];
     char kernel_version[150];
     char cpu[100];
     struct{
-        char usage[100];
-        int usage_kilobytes;
-        int usage_megabytes;
-        int usage_gigabytes;
+        struct{
+            char RAW[100];
+            int kB;
+            int mB;
+            int gB;
+        }total;
+        struct{
+            char RAW[100];
+            int kB;
+            int mB;
+            int gB;
+        }usage;
     }memory;
     struct{
-        char raw[100];
+        char RAW[100];
         int seconds;
         int minutes;
         int hours;
@@ -55,14 +64,13 @@ void convert_to_readable(int *small_unit, int *mid_unit, int *large_unit, int ma
     }
 }
 
-void get_uptime(char *filename, char *data){
-
+void get_everything(char *filename, char *data, int size){
     FILE *pointer = fopen(filename, "r");
     if (pointer == NULL) {
         printf("Error Opening File \n");
         return; //same as the first function, probably should've separated this
     }
-    fgets(data, sizeof(data), pointer);
+    fgets(data, size, pointer);
 
     if (fclose(pointer) == EOF){
         printf("Error Closing File");
@@ -76,37 +84,49 @@ int main() {
     char name_needle[12] = "PRETTY_NAME";
     char kernel_needle[14] = "Linux version";
     char cpu_needle[11] = "model name";
-    char memory_needle[7] = "Active";
+    char total_memory_needle[9] = "MemTotal";
+    char memory_usage_needle[7] = "Active";
 
-    //OS VERSION BLOCK - - - - - - - - - - - - - -
+    // HOSTNAME BLOCK - - - - - - - - - - - - - - - - - - -
+    get_everything("/etc/hostname", os.hostname, sizeof(os.hostname));
+    printf(BCYN "Hostname: " MAG "%s", os.hostname);
+
+    // OS VERSION BLOCK - - - - - - - - - - - - - - - - - -
     get_info("/etc/os-release", name_needle, os.name, sizeof(os.name));
     char *os_name_cleanup = (strstr(os.name, "=")) + 2;
     char *os_char_toRemove = strrchr(os.name, '"');
     *os_char_toRemove = '\0';
     printf(BCYN "Operating System: " MAG "%s \n", os_name_cleanup);
 
-    //KERNEL VERSION BLOCK - - - - - - - - - - -
+    // KERNEL VERSION BLOCK - - - - - - - - - - - - - - - -
     get_info("/proc/version", kernel_needle, os.kernel_version, sizeof(os.kernel_version));
     char *kernel_toRemovefrom = (strstr(os.kernel_version, "(")) - 1; //Best way i found to cut out the random data that follows the kernel release
     *kernel_toRemovefrom = '\0';
     char *kernel_cleanup = (strstr(os.kernel_version, "n ")) + 2; //best way i found to cut out "Linux Version"
     printf(BCYN "Kernel Version: " MAG "%s \n", kernel_cleanup);
 
-    //CPU MODEL BLOCK - - - - - - - - - - - - -
+    // CPU MODEL BLOCK - - - - - - - - - - - - - - - - - -
     get_info("/proc/cpuinfo", cpu_needle, os.cpu, sizeof(os.cpu));
     char *cpu_cleanup = (strstr(os.cpu, ":")) + 2;
     printf(BCYN "CPU model: " MAG "%s", cpu_cleanup);
 
-    //MEMORY USAGE BLOCK - - - - - - - - - - -
-    get_info("/proc/meminfo", memory_needle, os.memory.usage, sizeof(os.memory.usage));
-    sscanf(os.memory.usage, "Active:          %d", &os.memory.usage_kilobytes); //this extracts the kilobytes value
-    convert_to_readable(&os.memory.usage_kilobytes, &os.memory.usage_megabytes, &os.memory.usage_gigabytes, 1024); //no using google to figure out your ram usage
-    printf(BCYN "Memory Usage: " MAG "%d GB %d MB %d KB \n", os.memory.usage_gigabytes, os.memory.usage_megabytes, os.memory.usage_kilobytes);
+    // MEMORY BLOCK - - - - - - - - - - - - - - - - - - - -
+    // Total Memory
+    get_info("/proc/meminfo", total_memory_needle, os.memory.total.RAW, sizeof(os.memory.total.RAW));
+    sscanf(os.memory.total.RAW,"MemTotal:        %d", &os.memory.total.kB); // We filter out inncesary stuff to convert it into an integer
+    convert_to_readable(&os.memory.total.kB, &os.memory.total.mB, &os.memory.total.gB, 1024); //It's store in Kb's so for better readability for the user, they're converted into bigger units
 
-    get_uptime("/proc/uptime", os.uptime.raw);
-    sscanf(os.uptime.raw, "%d", &os.uptime.seconds);
-    convert_to_readable(&os.uptime.seconds, &os.uptime.minutes, &os.uptime.hours, 60);
-    printf(BCYN "Uptime: " MAG "%d h %d m %d s \n", os.uptime.hours, os.uptime.minutes, os.uptime.seconds); //wow only one line for this one
+    // Memory Usage
+    get_info("/proc/meminfo", memory_usage_needle, os.memory.usage.RAW, sizeof(os.memory.usage.RAW)); //It's the same process as the total memory sub-block
+    sscanf(os.memory.usage.RAW, "Active:          %d", &os.memory.usage.kB);
+    convert_to_readable(&os.memory.usage.kB, &os.memory.usage.mB, &os.memory.usage.gB, 1024);
+    printf(BCYN "Memory Usage: " MAG "%dgB %dmB %dkB of " GRN"%dgB %dmB %dkB total\n", os.memory.usage.gB, os.memory.usage.mB, os.memory.usage.kB, os.memory.total.gB, os.memory.total.mB, os.memory.total.kB); //now we display everything
+
+    //UPTIME BLOCK - - - - - - - - - - - - - -
+    get_everything("/proc/uptime", os.uptime.RAW, sizeof(os.uptime.RAW)); //I use get_everything when the file it's only storing the value and we don't need to iterate on it
+    sscanf(os.uptime.RAW, "%d", &os.uptime.seconds); // Same as the memory block, but to filter out a second value that's not relevant for this program, the important it's the first value
+    convert_to_readable(&os.uptime.seconds, &os.uptime.minutes, &os.uptime.hours, 60);//Like with memory, convert seconds into bigger units to be able to quickly read it
+    printf(BCYN "Uptime: " MAG "%d h %d m %d s \n", os.uptime.hours, os.uptime.minutes, os.uptime.seconds);
 
     printf(DFL);
     return 0;
